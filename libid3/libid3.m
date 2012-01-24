91d0b5e6234015f037cb7ed18808718a50962f8c
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2012-01-24 15:19:44 +0900

    Initial Commit

---
+++ libid3/libid3.m
@@ -0,0 +1,23 @@
+//
+//  libid3.m
+//  libid3
+//
+//  Created by 徐 楽楽 on 12/01/24.
+//  Copyright (c) 2012年 ラクラクテクノロジーズ. All rights reserved.
+//
+
+#import "libid3.h"
+
+@implementation libid3
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
